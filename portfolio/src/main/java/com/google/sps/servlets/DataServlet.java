0ae3d510c17bdc4693ae3c7e6fd79e87d76133cf
jcul22 <jculbreath@google.com>
2020-06-11 22:43:11 +0000

    Pushing changes to get help from Danny

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,10 +23,23 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+    
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hey Jaz!");
+    ArrayList<String> comments = new ArrayList<String>();
+    comments.add("Black Lives Matter!"); 
+    comments.add("The beginnings of a nice website, Jaz!");
+    comments.add("Website still needs some work but it's coming together! Must be learning some HTML/CSS."); 
+    String json = convertToJsonUsingGson(comments);    
+    
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJsonUsingGson(ArrayList<String> comments){
+      Gson gson = new Gson();
+      String json = gson.toJson(comments);
+      return json;
+
   }
 }
