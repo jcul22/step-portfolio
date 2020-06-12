515e1beaa3885df185bf55d3dbb4c28e1bf2a29a
jcul22 <jculbreath@google.com>
2020-06-12 21:03:38 +0000

    Changes names of functions and addresses other comments.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,20 +15,18 @@ public class DataServlet extends HttpServlet {
     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    ArrayList<String> comments = commentGenerator();
+    ArrayList<String> comments = getFakeComments();
     String json = toJsonString(comments);    
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
-
   private String toJsonString(ArrayList<String> comments){
       Gson gson = new Gson();
       String json = gson.toJson(comments);
       return json;
 
   }
-
-  public ArrayList<String> commentGenerator(){
+  public ArrayList<String> getFakeComments(){
     ArrayList<String> comments = new ArrayList<String>();
     comments.add("Black Lives Matter!"); 
     comments.add("The beginnings of a nice website, Jaz!");
